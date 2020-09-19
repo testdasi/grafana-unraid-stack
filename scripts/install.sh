96c3ed2ffbae5130b872a5d2218a73144bc982f8
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-19 23:08:19 +0100

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+# replace grafana service
+rm -f /etc/init.d/grafana-server \
+    && cp /temp/grafana-server /etc/init.d/ \
+    && chmod +x /etc/init.d/grafana-server
+
 # chmod scripts
 chmod +x /*.sh
