0de99e2b2e653e79ab705fc1fe8a2401bbb0c9c0
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-19 23:15:31 +0100

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+### Fixing config files ###
+echo ''
+echo '[info] Fixing configs'
+source /fix_config.sh
+echo '[info] All configs fixed'
+
+### Grafana ###
+echo ''
+echo "[info] Run grafana in background on port $GRAFANA_PORT"
+service grafana-server start
+
 ### Infinite loop to stop docker from stopping ###
 while true
 do
