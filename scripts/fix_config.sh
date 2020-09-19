89f6f20eb2519d1d19a00945561f59fce5359949
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-20 00:43:12 +0100

    Update fix_config.sh

--- scripts/fix_config.sh
+++ scripts/fix_config.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+mkdir -p /config/influxdb \
+    && cp -n /temp/influxdb.conf /config/influxdb/
+echo '[info] influxdb fixed.'
+
+mkdir -p /config/hddtemp \
+    && cp -n /temp/hddtemp.db /config/hddtemp/
+echo '[info] hddtemp fixed.'
+
+mkdir -p /config/telegraf/telegraf.d \
+    && echo 'telegraf.conf TBC - still need to work on this'
+echo '[info] telegraf fixed.'
+
 mkdir -p /config/grafana/data/plugins \
     && mkdir -p /config/grafana/log \
     && mkdir -p /config/grafana/provisioning/dashboards \
@@ -9,5 +21,4 @@ mkdir -p /config/grafana/data/plugins \
     && cp -n /temp/grafana.ini /config/grafana/
 echo '[info] grafana fixed.'
 
-mkdir -p /config/telegraf/telegraf.d
-echo '[info] telegraf fixed.'
+
