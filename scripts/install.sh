8f9ff33f29d2eeaa518ed2adfef811abb5e10a6f
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-20 00:06:14 +0100

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,5 +5,10 @@ rm -f /etc/init.d/grafana-server \
     && cp /temp/grafana-server /etc/init.d/ \
     && chmod +x /etc/init.d/grafana-server
 
+# replace influxdb service
+rm -f /etc/init.d/influxdb \
+    && cp /temp/influxdb /etc/init.d/ \
+    && chmod +x /etc/init.d/influxdb
+
 # chmod scripts
 chmod +x /*.sh
